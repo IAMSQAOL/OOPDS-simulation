cca719b93c62b183368b74ad8e902fc1d039efa7
IAMSQAOL <mo73977@gmail.com>
2025-02-05 12:57:04 +0800

    yes!

--- main.cpp
+++ main.cpp
@@ -1,8 +1,23 @@
+#include "OUTPUT.H"
 #include <iostream>
-#include "./include/Simulation.h"
+#include <cstdlib>
+#include<ctime>
+#include "SIMULATION.H"
 
 int main(){
-    srand(time(0));
-    Simulation sim;
+     try {
+        openF("output.txt");
+
+        mof << "Program started" << std::endl;
+
+        mof << "Processing..." << std::endl;
+        mof << std::endl;
+        srand(time(0));
+        Simulation sim;
+
+        close();
+    } catch (const std::exception& e) {
+        std::cerr << "Error: " << e.what() << std::endl;
+    }
     return 0;
 }
